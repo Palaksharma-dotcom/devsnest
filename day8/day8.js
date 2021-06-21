fb2f15c56a17fdcf24b48e01335df900af9a1150
Palaksharma-dotcom <70392674+Palaksharma-dotcom@users.noreply.github.com>
2021-06-21 17:49:42 +0530

    Create day8.js

---
+++ day8/day8.js
@@ -0,0 +1,19 @@
+var row='<tr>'
+for(i=0;i<20;i++){
+    row+='<td></td>'
+}
+row+='</tr>'
+console.log(row)
+var content='';
+for(i=0;i<20;i++)
+{
+    content+=row;
+}
+document.getElementById('tab').innerHTML=content
+let cell=document.querySelectorAll('td');
+cell.forEach((link)=>{
+    link.addEventListener("click",function () {
+        link.classList.toggle("white");
+        
+    })
+});
