81a60b5808384729861e618d0aa4ceb45b749584
Palaksharma-dotcom <70392674+Palaksharma-dotcom@users.noreply.github.com>
2021-07-11 21:52:12 +0530

    Add files via upload

---
+++ Day16/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card=()=>{
+    return(
+        <div className="Card">
+            <img src="https://www.probytes.net/wp-content/uploads/2018/01/5-1.png" alt="" />
+            <h2>this is a meme card</h2>
+        </div>
+    );
+};
+export default Card;
